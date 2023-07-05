a1b7a047179c9bca3eba45e1a4c0bb18e8b530df
Sadi Júnior <sadilemesjr@gmail.com>
2023-07-05 20:52:42 -0300

    added the home page

---
+++ site.js
@@ -0,0 +1,12 @@
+
+AOS.init();
+
+var typed = new Typed(
+    ".auto-type", {
+        strings: ["Software Developer.", "Musician.", "Student.", "Reader."],
+        startDelay: 2000,
+        backDelay: 1000,
+        typeSpeed: 150,
+        backSpeed: 100,
+        loop: true,
+    });
