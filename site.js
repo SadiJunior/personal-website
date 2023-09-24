9766bff22515e00a5924cb8c62153999bbad5fd8
Sadi Júnior <sadilemesjr@gmail.com>
2023-09-24 01:58:40 -0300

    added modal Sugar image

--- site.js
+++ site.js
@@ -10,3 +10,53 @@ var typed = new Typed(
         backSpeed: 100,
         loop: true,
     });
+
+const modal = document.getElementById("sugar-modal-image");
+const openButton = document.getElementById("open-sugar-image");
+const closeButton = document.getElementById("close-button");
+
+openButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.style.display = "flex";
+        disableScroll();
+        disableTab();
+        modal.focus();
+    });
+
+closeButton.addEventListener("click", function () {
+        modal.style.display = "none";
+        enableScroll();
+        enableTab();
+        openButton.focus();
+    });
+
+window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+            enableScroll();
+            enableTab();
+            openButton.focus();
+        }
+    });
+
+function enableScroll()   {
+    document.body.style.overflow = "auto";
+}
+
+function disableScroll() {
+    document.body.style.overflow = "hidden";
+}
+
+function enableTab() {
+    document.removeEventListener("keydown", blockTab);
+}
+
+function disableTab() {
+    document.addEventListener("keydown", blockTab);
+}
+
+function blockTab(event) {
+    if (event.key === "Tab") {
+        event.preventDefault();
+    }
+}
